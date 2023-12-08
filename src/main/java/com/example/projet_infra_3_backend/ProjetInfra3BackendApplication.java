094f3d38fc4fe93d83a91925efe3cdcc7332f3a2
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2023-12-08 11:41:57 +0100

    first commit

---
+++ src/main/java/com/example/projet_infra_3_backend/ProjetInfra3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_infra_3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetInfra3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetInfra3BackendApplication.class, args);
+    }
+
+}
