7aed5006dac8487b8283498abe0d83a0c70fd125
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2023-12-23 12:03:09 +0100

    Swagger fonctionnelle

--- src/main/java/com/example/projet_infra_3_backend/config/SwaggerConfig.java
+++ src/main/java/com/example/projet_infra_3_backend/config/SwaggerConfig.java
@@ -2,17 +2,15 @@ package com.example.projet_infra_3_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.example.projet_infra_3_backend.constant.SwaggerRoot.APP_ROOT;
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
     @Bean
     public Docket api() {
--- src/main/java/com/example/projet_infra_3_backend/constant/SwaggerRoot.java
+++
@@ -1,5 +0,0 @@
-package com.example.projet_infra_3_backend.constant;
-
-public class SwaggerRoot {
-    public static final String APP_ROOT = "monclasseur/v1";
-}
