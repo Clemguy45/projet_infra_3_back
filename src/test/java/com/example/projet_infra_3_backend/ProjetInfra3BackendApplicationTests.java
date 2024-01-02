91f2089cabb4d7ea9bf7adc4d7b7f26e9f139336
Clemguy45 <clement.guyot@etu.univ-orleans.fr>
2024-01-02 23:50:56 +0100

    Mise en place des test

--- src/test/java/com/example/projet_infra_3_backend/ProjetInfra3BackendApplicationTests.java
+++ src/test/java/com/example/projet_infra_3_backend/ProjetInfra3BackendApplicationTests.java
@@ -7,13 +7,25 @@ import com.example.projet_infra_3_backend.exception.UsernameExistException;
 import com.example.projet_infra_3_backend.modele.User;
 import com.example.projet_infra_3_backend.service.UserService;
 import com.example.projet_infra_3_backend.service.UserServiceImpl;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -23,10 +35,9 @@ import org.springframework.test.context.TestPropertySource;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.data.mongodb.uri = mongodb+srv://projetinfra3user:projetinfra3password@projetinfra3.pslp1fj.mongodb.net/?retryWrites=true&w=majority"
-})
 class ProjetInfra3BackendApplicationTests {
 
 }
